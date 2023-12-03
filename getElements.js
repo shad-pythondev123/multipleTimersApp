6dce9c3355af2168054a90fcdbddc5d30a557e11
shad <princekhan785@gmail.com>
2023-12-03 22:08:59 +0530

    finalcommit

---
+++ getElements.js
@@ -0,0 +1,7 @@
+const setButton = document.getElementById("set-btn");
+const activeTimersContainer = document.getElementById(
+  "active-timers-container"
+);
+const noTimerDisplayMessage = document.getElementById("no-timers-display-msg");
+const audio = document.getElementById("audio-file");
+let numberOfActiveTimers = 0;
